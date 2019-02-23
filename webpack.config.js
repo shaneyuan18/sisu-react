1e14aa5cea478e9a28220a6f8f32d312c6900bad
Shihhsuan Yuan <shihyuan@paypal.com>
2019-02-22 19:20:16 -0800

    Allow css Modules to work with bootstrap-react

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,19 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: [
+          {
+             loader: 'style-loader',
+          },
+          {
+             loader: 'css-loader',
+             options: {
+                sourceMap: true,
+                modules: true,
+                localIdentName: '[local]___[hash:base64:5]'
+               }
+          }
+        ],
       },
     ],
   },
